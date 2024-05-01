d8b382e01229810b56b5e1df2ed2734b03ea1de4
ayushmakafle <amikafle1@gmail.com>
2024-05-01 21:09:09 +0545

    card responsive

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -15,8 +15,8 @@ const BookCard = ({ book }) => {
   return (
     <div
       className="card m-2 border border-gray-300 rounded-lg hover:shadow-2xl"
-      style={{ width: "18rem" }}
       onClick={handleClick}
+      style={{ width: "18rem" }}
     >
       <div className="overflow-hidden rounded-t-lg h-64">
         <img
@@ -26,17 +26,17 @@ const BookCard = ({ book }) => {
         />
       </div>
       <div className="card-body">
-        <h5 className="card-title text-2xl font-bold text-pink-800 mb-2 p-4">
+        <h5 className="card-title text-2xl font-bold text-pink-800 mb-2 p-4 h-20">
           {book.name}
         </h5>
         <p className="card-text mb-2 text-lg text-pink-900 px-4">
           {book.author.name}
         </p>
         <p className="price mb-2 font-bold text-xl px-4">$ {book.price}</p>
-        <div className="buttons flex gap-3 px-4 text-lg mb-4">
-          <button className="bg-pink-800 text-white py-1 mb-2 rounded-xl p-4">
+        <div className="buttons flex justify-end gap-4 px-4 text-lg mb-4">
+          {/* <button className="bg-pink-800 text-white py-1 mb-2 rounded-xl p-4">
             More Details
-          </button>
+          </button> */}
           <button className="bg-pink-800 text-white px-2 py-1 mb-2 rounded-xl">
             <FontAwesomeIcon icon={faCartPlus} />
           </button>
